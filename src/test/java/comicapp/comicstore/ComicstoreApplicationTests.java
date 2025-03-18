7fd58c693a169b8e9587245ad22532f667ea6a1d
Sergi <sergi.prof@hotmail.com>
2025-03-18 13:04:52 +0100

    feat: springboot basic structure

---
+++ src/test/java/comicapp/comicstore/ComicstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package comicapp.comicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
